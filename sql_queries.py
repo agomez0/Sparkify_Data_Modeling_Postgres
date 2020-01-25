ef73b050a8f19ba09645459b5f42dbd4332f7a80
Angie <agomez0041@gmail.com>
2020-01-25 10:06:39 -0800

    update sql queries

---
+++ sql_queries.py
@@ -0,0 +1,50 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLBE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""CREATE TABLE songplays (songplay_id int, start_time time, user_id int,
+level int, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);""")
+
+user_table_create = ("""CREATE TABLE users (user_id int, first_name varchar, last_name varchar,
+gender varchar, level int);""")
+
+song_table_create = ("""CREATE TABLE songs (song_id varchar, title varchar, artist_id varchar, year int, duration int);""")
+
+artist_table_create = ("""CREATE TABLE artists (artist_id varchar, name varchar, location varchar,
+latitude numeric, longitude numeric);""")
+
+time_table_create = ("""CREATE TABLE time (start_time time, hour int, day varchar, week varchar, month varchar,
+year int, weekday varchar);""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+
+time_table_insert = ("""
+""")
+
+# FIND SONGS
+
+song_select = ("""SELECT * FROM songs;""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
